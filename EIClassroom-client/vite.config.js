414ff7e5e00d86205c3530355ff37ea4d24d090c
Pranshu Pandey <pranshu0604@gmail.com>
2024-12-13 02:44:53 +0530

    revert

--- EIClassroom-client/vite.config.js
+++ EIClassroom-client/vite.config.js
@@ -1,10 +1,7 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
-import checker from 'vite-plugin-checker';
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
+// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [
-    react(),
-    checker({ typescript: true }),
-  ],
-});
+  plugins: [react()],
+})
