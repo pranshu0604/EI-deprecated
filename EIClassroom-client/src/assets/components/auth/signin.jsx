c5a1174b69cefac1220d3e34e52e196b16639d91
Suryansh Singh <141821325+suryxnshsingh@users.noreply.github.com>
2024-12-13 17:04:21 +0530

    fix

--- EIClassroom-client/src/assets/components/auth/signin.jsx
+++ EIClassroom-client/src/assets/components/auth/signin.jsx
@@ -141,7 +141,7 @@ const Signin = () => {
           {success && <p className="text-green-500 text-center pb-2">{success}</p>}
 
           <button
-            className="bg-gradient-to-br relative group/btn from-black dark:from-zinc-900 dark:to-zinc-900 to-neutral-600 block dark:bg-zinc-800 w-full text-white rounded-md h-10 font-medium shadow-[0px_1px_0px_0px_#ffffff40_inset,0px_-1px_0px_0px_#ffffff40_inset] dark:shadow-[0px_1px_0px_0px_var(--zinc-800)_inset,0px_-1px_0px_0px_var(--zinc-800)_inset] flex items-center justify-center"
+            className="bg-gradient-to-br relative group/btn from-black dark:from-zinc-900 dark:to-zinc-900 to-neutral-600 dark:bg-zinc-800 w-full text-white rounded-md h-10 font-medium shadow-[0px_1px_0px_0px_#ffffff40_inset,0px_-1px_0px_0px_#ffffff40_inset] dark:shadow-[0px_1px_0px_0px_var(--zinc-800)_inset,0px_-1px_0px_0px_var(--zinc-800)_inset] flex items-center justify-center"
             type="submit"
             disabled={buttonLoading} // Disable button when loading
           >
